24a7f114e44ba20d2cd0069af3e67972560a933a
skyPad <ab@cd.de>
2022-01-05 04:25:39 +0100

    dreame device started

--- 02-process.sh
+++ 02-process.sh
@@ -2,8 +2,11 @@
 #
 # Normalize the volume of the spoken text.
 # Requires ffmpeg-normalize (pip install ffmpeg-normalize) and ffmpeg (http://ffmpeg.org/download.html).
+# For Dreame devices convert wav to ogg files
 
 inputdir=output/speech
 outputdir=output/result
 
 ffmpeg-normalize $inputdir/*.wav --normalization-type peak --target-level 0 -of $outputdir -ext wav
+
+ffmpeg -i $outputdir/*.wav -acodec libvorbis  $outputdir/*.ogg
