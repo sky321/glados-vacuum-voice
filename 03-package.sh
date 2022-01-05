40baafffa607f67adcbe28d05d4e76f51aa8a15f
skyPad <ab@cd.de>
2022-01-05 04:24:16 +0100

    dreame devices support started

--- 03-package.sh
+++ 03-package.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 #
 # Package wav files in a directory to embed in a dustcloud image or upload to Valetudo for xiaomi gen1 and gen2.
-# Requires ccrypt.
+# Requires ccrypt, md5sum
 
 dir=output/result
 sound_password="r0ckrobo#23456"
 
-mkdir -p $dir
-
 cd $dir
-tar zc *.wav | ccrypt -e -K "$sound_password" > voicepack.pkg
+
+# for roborock pack like this
+tar zc *.wav | ccrypt -e -K "$sound_password" > robo-voicepack.pkg
+
+# for dreame pack like this
+tar -czvf dreame-voicepack.tar.gz *.ogg
+md5sum dreame-voicepack.tar.gz > dreame-voicepack.md5
